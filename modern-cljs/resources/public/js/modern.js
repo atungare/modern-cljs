54b8226cc03abf771a6a3b1d84e2f4e44624c273
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-17 23:52:42 -0700

    form validation and repl stuff

--- modern-cljs/resources/public/js/modern.js
+++ modern-cljs/resources/public/js/modern.js
@@ -28465,6 +28465,34 @@ goog.net.xpc.Transport.prototype.getName = function() {
 goog.net.xpc.Transport.prototype.transportServiceHandler = goog.abstractMethod;
 goog.net.xpc.Transport.prototype.connect = goog.abstractMethod;
 goog.net.xpc.Transport.prototype.send = goog.abstractMethod;
+goog.provide("modern_cljs.login");
+goog.require("cljs.core");
+modern_cljs.login.validate_form = function validate_form() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  if(cljs.core.count.call(null, email.value) > 0 && cljs.core.count.call(null, password.value) > 0) {
+    return true
+  }else {
+    alert("Please complete the form");
+    return false
+  }
+};
+modern_cljs.login.init = function init() {
+  if(cljs.core.truth_(function() {
+    var and__3273__auto__ = document;
+    if(cljs.core.truth_(and__3273__auto__)) {
+      return document.getElementById
+    }else {
+      return and__3273__auto__
+    }
+  }())) {
+    var login_form = document.getElementById("loginForm");
+    return login_form.onsubmit = modern_cljs.login.validate_form
+  }else {
+    return null
+  }
+};
+window.onload = modern_cljs.login.init;
 goog.provide("goog.messaging.MessageChannel");
 goog.messaging.MessageChannel = function() {
 };
