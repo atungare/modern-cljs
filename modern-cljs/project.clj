88ebd4a0010e9648330fbab9c82734f36a2b2ee5
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-17 21:57:07 -0700

    tutorial 1.5ish

--- modern-cljs/project.clj
+++ modern-cljs/project.clj
@@ -3,4 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2069"]]
+
+  ;; CLJ and CLJS source code path
+  :source-paths ["src/clj" "src/cljs"]
+
+  ;; lein-cljsbuild
+  :plugins [[lein-cljsbuild "1.0.0"]]
+
+  :cljsbuild {:builds
+              [{;; CLJS source paths
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options
+                :compiler {;; js path
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal optimizations
+                           :optimizations :whitespace
+
+                           :pretty-print true}}]})
+
