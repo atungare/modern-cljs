1ba9f0cc0f35dd654cc6959243219c466ef5ef52
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-17 22:13:36 -0700

    tutorial 2, brepl enabled

--- modern-cljs/resources/public/js/modern.js
+++ modern-cljs/resources/public/js/modern.js
@@ -30741,4 +30741,4 @@ goog.provide("modern_cljs.connect");
 goog.require("cljs.core");
 goog.require("clojure.browser.repl");
 goog.require("clojure.browser.repl");
-repl.connect.call(null, "http:localhost:9000/repl");
+clojure.browser.repl.connect.call(null, "http://localhost:9000/repl");
