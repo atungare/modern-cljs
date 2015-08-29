cfabeefdfc7b55ec1ad7d0e12350abb127f34691
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-28 21:20:01 -0700

    tutorial 12: progressive enhancement

--- modern-cljs/src/clj/modern_cljs/core.clj
+++ modern-cljs/src/clj/modern_cljs/core.clj
@@ -1,17 +1,19 @@
 (ns modern-cljs.core
-  (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
-            [compojure.route :as route]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.handler :refer [site]]
+            [compojure.route :refer [resources not-found]]
+            [modern-cljs.login :refer [authenticate-user]]))
 
 ;; defroutes is a macro that defines a function that chains handler
 ;;   functions in sequence and returns first non-nil value
 (defroutes app-routes
   ;; root
   (GET "/" [] "<p>Hello, World, from Compojure!</p>")
+  (POST "/login" [email password] (authenticate-user email password))
   ;; static assets
-  (route/resources "/")
-  (route/not-found "Page not found."))
+  (resources "/")
+  (not-found "Page not found."))
 
 ;; site adds boilerplate middleware
 (def handler
-  (handler/site app-routes))
+  (site app-routes))
