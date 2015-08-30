1f5ff13b1803c68da09584a6fdea9ba6b51b3656
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-30 16:27:36 -0700

    tutorial 16 step 1

--- modern-cljs/project.clj
+++ modern-cljs/project.clj
@@ -16,11 +16,12 @@
   ;; CLJ and CLJS source code path
   :source-paths ["src/clj" "src/cljs" "src/brepl"]
 
-  :test-paths ["test/clj"]
+  :test-paths ["test/clj" "test/cljs"]
 
   ;; lein-cljsbuild
   :plugins [[lein-cljsbuild "1.0.0"]
-            [lein-ring "0.8.8"]]
+            [lein-ring "0.8.8"]
+            [com.cemerick/clojurescript.test "0.2.1"]]
 
   :hooks [leiningen.cljsbuild]
 
@@ -63,7 +64,54 @@
                            ;; killing of unused code
                            :optimizations :advanced
 
-                           :pretty-print false}}}}
+                           :pretty-print false}}
+               :ws-unit-tests
+               {;; CLJS source code and unit test paths
+                :source-paths ["src/brepl" "src/cljs" "test/cljs"]
+
+                ;; Google Closure Compiler options
+                :compiler {;; the name of emitted JS script file for unit testing
+                           :output-to "test/js/testable_dbg.js"
+
+                           ;; minimum optimization
+                           :optimizations :whitespace
+                           ;; prettyfying emitted JS
+                           :pretty-print true}}
+
+               :simple-unit-tests
+               {;; same path as above
+                :source-paths ["src/brepl" "src/cljs" "test/cljs"]
+
+                :compiler {;; different JS output name for unit testing
+                           :output-to "test/js/testable_pre.js"
+
+                           ;; simple optimization
+                           :optimizations :simple
+
+                           ;; no need prettification
+                           :pretty-print false}}
+
+               :advanced-unit-tests
+               {;; same path as above
+                :source-paths ["src/cljs" "test/cljs"]
+
+                :compiler {;; different JS output name for unit testing
+                           :output-to "test/js/testable.js"
+
+                           ;; advanced optimization
+                           :optimizations :advanced
+
+                           ;; no need prettification
+                           :pretty-print false}}}
+
+              :test-commands {"phantomjs-whitespace"
+                              ["phantomjs" :runner "test/js/testable_dbg.js"]
+
+                              "phantomjs-simple"
+                              ["phantomjs" :runner "test/js/testable_pre.js"]
+
+                              "phantomjs-advanced"
+                              ["phantomjs" :runner "test/js/testable.js"]}}
 
   :ring {:handler modern-cljs.core/app})
 
